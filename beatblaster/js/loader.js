438d88ded930db0bbaccdfed7d6a42ec04a88a60
aisopuro <atte.isopuro@aalto.fi>
2013-12-10 15:16:21 +0200

    Added Queue.js

--- beatblaster/js/loader.js
+++ beatblaster/js/loader.js
@@ -1,9 +1,11 @@
-// Class for handling file loading
+// Class for handling file loading. Returns a list of LoadQueue item-objects as 
+// an argument to the specified callback function
 function Loader( callback ) {
     console.log( "init loader" );
     this.callback = callback;
     this.callbackName = "callbackName";
     this.queue;
+    this.loadedAssets = [];
     this.useXHR = ( window.ON_SERVER ) ? true : false;
     var loader = this;
     // Load resources
@@ -20,6 +22,10 @@ function Loader( callback ) {
             }
         } );
 }
+
+Loader.prototype.loadComplete = function() {
+    this.callback( this.loadedAssets );
+};
 
 Loader.prototype.fileLoaded = function( event ) {
     var item = event.item;
@@ -45,20 +51,18 @@ Loader.prototype.loadLibraries = function() {
     // Build queue
     // Using separate builder functions for this did not work: 
     // have to build the queue here
-    this.queue.loadManifest( [ {
-        id: "easeljs",
-        src: "./beatblaster/js/easeljs-0.7.0.min.js",
-        type: createjs.LoadQueue.JAVASCRIPT
-    }, {
-        id: "soundjs",
-        src: "./beatblaster/js/soundjs-0.5.0.min.js",
-        type: createjs.LoadQueue.JAVASCRIPT
-    }, 
-    {
-        id: "soundhandler",
-        src: "./beatblaster/js/soundhandler.js",
-        type: createjs.LoadQueue.JAVASCRIPT
-    } ] );
+    var basepath = "./beatblaster/js/"
+    this.queue.loadManifest( [
+            "easeljs-0.7.0.min.js",
+            "soundjs-0.5.0.min.js",
+            "soundhandler.js",
+            "builder.js",
+            "constants.js",
+            "gamemanager.js",
+            "Queue.compressed.js"
+        ], 
+        true,
+        basepath );
     console.log( "Queue built" );
 };
 
@@ -68,16 +72,15 @@ Loader.prototype.librariesLoaded = function( event ) {
 };
 
 Loader.prototype.loadAssets = function() {
-    this.queue.close();
-    this.queue = new createjs.LoadQueue( this.useXHR );
+    this.queue.removeAllEventListeners();
 
     this.queue.addEventListener( "fileload", this.assetLoaded.bind( this ) );
     this.queue.addEventListener( "error", this.loadError.bind( this ) );
     this.queue.addEventListener( "complete", this.assetsLoaded.bind( this ) );
 
     this.queue.loadManifest( [ {
-        id: "playerSpriteheet",
-        src: "./beatblaster/img/spaceshipsprites.gif",
+        id: Constants.IMAGE_ID_PLAYER,
+        src: Constants.PATH_TO_PLAYER_IMAGE,
         type: createjs.LoadQueue.IMAGE,
         data: {
             // Data about the sprite sheet (frame size, animations)
@@ -91,19 +94,23 @@ Loader.prototype.assetLoaded = function( event ) {
     console.log( event.item );
     var item = event.item;
     var data = item.data;
-    if (data === undefined || data === null) {
+    if ( data === undefined || data === null ) {
         data = {
             type: ""
         }
     }
-    if (item.type === createjs.LoadQueue.IMAGE) {
-        console.log("Loaded image");
-        if (data.type === "spritesheet") {
-            console.log("Image was a spritesheet");
+    if ( item.type === createjs.LoadQueue.IMAGE ) {
+        console.log( "Loaded image" );
+        if ( data.type === "spritesheet" ) {
+            console.log( "Image was a spritesheet" );
+            this.loadedAssets.push( item );
         }
+    } else if ( item.type === createjs.LoadQueue.SOUND ) {
+        // Loaded sound
+        this.loadedAssets.push( item );
     }
 };
 
 Loader.prototype.assetsLoaded = function( event ) {
-    this.callback();
+    this.loadComplete();
 };
