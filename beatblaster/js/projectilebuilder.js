eb31798572dfc9cb96203c8cd1035f1745d66c07
aisopuro <atte.isopuro@aalto.fi>
2013-12-12 14:55:41 +0200

    Moved projectile construction to separate class.

---
+++ beatblaster/js/projectilebuilder.js
@@ -0,0 +1,33 @@
+// A class for building projectile entities
+ProjectileBuilder = {
+    build: function( beatType, parentImg ) {
+        var projectile;
+        projectile.beatType = beatType;
+        projectile.cursor = 0;
+        // Construct a projectile based on the associated beat type
+
+        if ( beatType === 0 ) { // Dummy
+            // Basic laser
+            projectile.move = function( image ) {
+                image.y = image.y - 10;
+            }
+
+            // Red, rounded rectangles
+            var image = new createjs.Shape();
+            image.graphics.beginFill( "#ff0000" ).drawRoundRect( 0, 0, 10, 50, 5 );
+            image.setBounds( 0, 0, 10, 50 );
+            image.x = -1000;
+            image.y = -1000;
+            for ( var i = 0; i < Constants.PROJECTILE_ARRAY_SIZE_LASER; i++ ) {
+                var currentClone = image.clone();
+                currentClone.active = false;
+                temp.images.push( currentClone );
+                parentImg.addChild( currentClone );
+            }
+        } else {
+            // Not a supported beatType
+            return null;
+        }
+        return projectile;
+    }
+}
