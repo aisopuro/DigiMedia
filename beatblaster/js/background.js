5f59ae2df132c4d044601c8abf5bfa98f4d11885
Sentri <daedalsmail@gmail.com>
2013-12-13 19:04:56 +0200

    Simple background

--- beatblaster/js/background.js
+++ beatblaster/js/background.js
@@ -1,3 +1,6 @@
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 function Background( stage, starcount, layers ) {
 	var bg = new createjs.Shape();
@@ -5,8 +8,32 @@ function Background( stage, starcount, layers ) {
 	bg.graphics.beginFill("#000").drawRect(0, 0, this.bounds.width, this.bounds.height);
 	stage.addChild(bg);
 	this.back = bg;	
+	
+	this.globaldampen = 0.3;	
+	this.layers = [];
+	
+	for (var l = 0; l < layers; l++) {
+		this.layers[l] = [];
+		for (var s = 0; s < starcount; s++) {
+			var star = new createjs.Shape();
+			star.graphics.beginFill("#FFF").drawCircle(0, 0, 1+l);
+			star.x = star.origx = getRandomInt(0, this.bounds.width);
+			star.y = star.origy = getRandomInt(0, this.bounds.height);
+			stage.addChild(star);
+			this.layers[l][s] = star;
+		}
+	}
+	
 }
 
 Background.prototype.updateStars = function(playerX, playerY){
-	
+	var lc = this.layers.length;
+	for (var l = 0; l < lc; l++) {
+		for (var s = 0; s < this.layers[l].length; s++) {
+			var star = this.layers[l][s];
+			var dampening = (1/lc) * l * this.globaldampen;
+			star.x = ( star.origx + (-playerX * dampening) ) % this.bounds.width;
+			star.y = ( star.origy + (-playerY * dampening) ) % this.bounds.height;
+		}
+	}
 }
