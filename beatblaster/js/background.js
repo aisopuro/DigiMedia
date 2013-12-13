07e4443c13fcf45d6698bbcd4c244758412cc71d
Sentri <daedalsmail@gmail.com>
2013-12-13 18:48:20 +0200

    Starting to draw

--- beatblaster/js/background.js
+++ beatblaster/js/background.js
@@ -1,8 +1,10 @@
 
 function Background( stage, starcount, layers ) {
-	
+	this.back = new createjs.Shape();
+	this.bounds = stage.getBounds();
+	this.back.graphics.beginFill("#000").drawRect(0, 0, this.bounds.width, this.bounds.height);
 }
 
-Background.prototype.updateStars(playerX, playerY) {
+Background.prototype.updateStars = function(playerX, playerY){
 	
 }
